b39b1e8491dafe207cc93c1efb1eaf69062cd39e
rahulrao011 <rahul.seth.rao@gmail.com>
2024-02-23 16:56:13 -0600

    test to see if deployment updates in real time

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import Card from '../components/Card'
 function Home() {
   return (
     <div className="Home">
-        <h1>Welcome to a new experience!!!</h1>
+        <h1>Welcome to a new experience V2!!!</h1>
         <Card cardId="1" title="Ice Skating" description="Ice skating club welcome event"/>
         <Card cardId="2" title="Break Dancing" description="WOW! FREE FOOD!!!"/>
         <Card cardId="3" title="Blindfold Skiing" description="Try not to die :)"/>
